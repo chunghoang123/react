7f4810404a8f5bb66ce9104ba9d187e7db63f077
chung <youremail@example.com>
2025-09-03 17:21:21 +0700

    Update project: remove session18, add session19 & session21

---
+++ react/session21/src/components/Bai4.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Bai4() {
+  return (
+    <div>
+      <button className="px-6 py-2 rounded-full text-white font-semibold bg-[#0ea5e9] hover:bg-[#0369a1] transition duration-300">
+        Submit
+      </button>
+    </div>
+  );
+}
