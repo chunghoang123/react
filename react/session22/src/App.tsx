8d5a8436a3bc6a7396e1a4247cd9cda0c03c8549
chung <youremail@example.com>
2025-09-04 20:40:51 +0700

    update

---
+++ react/session22/src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+// import Test from './components/Test'
+import Bai1 from "./components/Bai1";
+import Bai2 from "./components/Bai2";
+import Bai3 from "./components/Bai3";
+import Bai4 from "./components/Bai4";
+import Bai5 from "./components/Bai5";
+import Bai6 from "./components/bai6";
+
+function App() {
+  return (
+    <>
+      {/* <Test/> */}
+      <Bai1 />
+      <Bai2 />
+      <Bai3 />
+      <Bai4 />
+      <Bai5 />
+      <Bai6/>
+    </>
+  );
+}
+
+export default App;
